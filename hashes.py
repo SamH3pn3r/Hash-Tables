96c2fe666e2352931d58f49e0517ddf0f5768cfe
SamH3pn3r <samuelhepner03@gmail.com>
2020-02-10 16:16:46 -0800

    Completed MVP and made new py file with examples from class

---
+++ hashes.py
@@ -0,0 +1,13 @@
+import hashlib
+
+
+key = b"str"
+
+# for i in range(10):
+#     hashed = hashlib.sha256(key).hexdigest()
+#     print(hashed)
+
+for i in range(10):
+    hashed = hash(key)
+    print(hashed)
+    print(hashed % 8)
